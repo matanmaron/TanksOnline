e511254bac0f483c4daa029298cd2f307e6896b1
Matan Maron <matanmaron@gmail.com>
2020-04-11 20:12:03 +0300

    update 1

---
+++ Assets/Scripts/LogManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+
+public static class LogManager
+{
+    public static void LogDebug(string str)
+    {
+        StringBuilder output = new StringBuilder();
+        output.Append("[DEBUG] - [");
+        output.Append(DateTime.UtcNow);
+        output.Append("(UTC)] : ");
+        output.Append(str);
+        Debug.Log(output.ToString());
+    }
+    public static void LogError(string str)
+    {
+        StringBuilder output = new StringBuilder();
+        output.Append("[ERROR] - [");
+        output.Append(DateTime.UtcNow);
+        output.Append("(UTC)] : ");
+        output.Append(str);
+        Debug.Log(output.ToString());
+    }
+}
